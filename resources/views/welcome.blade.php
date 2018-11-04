b71f4fa98f018a2f8d43ac3226f7ba6cf6949c04
weihien90 <weihien90@gmail.com>
2018-11-05 00:22:50 +0800

    Show tenant's name in welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hello, {{ session('tenant')->name }}
                 </div>
 
                 <div class="links">
